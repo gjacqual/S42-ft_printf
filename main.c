a08f316254049964b59e84b992abdc8f1543234c
Gerold Jacqualine <gjacqual@student.21-school.ru>
2022-02-09 20:28:00 +0300

    update main.c

--- main.c
+++ main.c
@@ -3,29 +3,7 @@
 
 int main()
 {
-/* Thanks aaarnell for this test */
-    int r;
-    char *c = "abcdefgh13";
-    char *s[10] = {"abcd\0", "1234\0", NULL, "", "abcd\0", "1234\0", "dfge\0", "dffg\0", "abcd\0", "1234\0"};
-    int a[10] = {0, 2147483647, -2147483648, 3, -4, 5, -6, 7, -8, 9};
-    unsigned int u[10] = {0, 2147483647, 4294967295, -3, 4, 5, 6, 7, 8, 9};
-    int x[10] = {0, 2147483647, 429496729, -12, 4, -1000000, 6, 7, 11, 9};
-
-    printf("\n");
-
-    //printf("%s|\t \n", s[2]);
-    //ft_printf("%s|\t \n", s[2]);
-
-    for(int i = 0; i < 10; i++)
-    {
-		printf("Iteration %d\n", i);
-    	r = ft_printf("My : |%c|\t|%s|\t|%p|\t|%d|\t|%i|\t|%u|\t|%x|\t|%X|\t|%%|\t", c[i], s[i], &s[i], a[i], a[i], u[i], x[i], x[i]);
-        printf("res:%d\n", r);
-        r = printf("Orig:  |%c|\t|%s|\t|%p|\t|%d|\t|%i|\t|%u|\t|%x|\t|%X|\t|%%|\t", c[i], s[i], &s[i], a[i], a[i], u[i], x[i], x[i]);
-        printf("res:%d\n\n", r);
-    }
-/* end test */
-
+/* my tests */
 char cimb = 'R';
 char *str;
 str = "Пример строки";
